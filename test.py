920622ea1d6f6c412e2fbfbdb8199e4b09062336
aqoudayo <hiyeonu0520@gmail.com>
2024-03-24 01:02:40 +0900

    test

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("test")
+'''
+    Reference : https://velog.io/@rjsekaehdhkw/Visual-Studio-Code%EC%97%90%EC%84%9C-Django-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EA%B8%B0%EB%B3%B8-%EC%85%8B%ED%8C%85-%ED%95%98%EB%8A%94-%EB%B2%95
+    -------------------------------------
+    venv 가상환경
+    가상환경 실행 : venv/Scripts/activate
+    가상환경 종료 : deactivate
+    -------------------------------------
+    pip list에서  사용한 라이브러리 체크한 후 requirements.txt 에 업데이트 하기
+    -> 이러면 pip install -r requirements.txt로 다른사람이 쓸때 패키지 한번에 다운
+'''
+
+print("test")    
